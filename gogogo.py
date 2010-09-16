3a18731aa3b429b6c03f06bc718f94430907d0f1
Yaroslav Shirokov <Yaroslav.Shirokov@cmgdigital.com>
2010-09-16 02:13:38 -0400

    Validate path through init

--- gogogo.py
+++ gogogo.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import sys
 
+class BoardError(Exception): pass
+class HistoryInvalid(BoardError): pass
+
 class BoardState(object):
     def __init__(self, *players, **options):
         '''
@@ -9,15 +12,18 @@ class BoardState(object):
         options = dict({'width': 19,
                         'height': 19,
                         'players': players,
+                        'moves': [],
+                        'positions': [],
                         },
                        **options)
-        self.moves = options.pop('moves', {})
-        if not self.moves:
-            [self.moves.update({name: []}) for name in options['players']]
         [setattr(self, k, v) for (k, v) in options.items()]
+        if not self.validate(): raise HistoryInvalid()
+
+    def validate(self):
+        return True
 
     def __str__(self):
-        return u"<Board: (%d, %d) :: (%s) Moves: %d>" % (self.width, self.height, " vs ".join(self.players), sum([len(m) for (n, m) in self.moves.items()]))
+        return u"<Board: (%d, %d) :: (%s) Moves: %d>" % (self.width, self.height, " vs ".join(self.players), len(self.moves))
     __unicode__ = __str__
 
 def main(self, *args):
