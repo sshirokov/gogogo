c536ae638d751bb247f459c2ad87433c815cd626
Yaroslav Shirokov <Yaroslav.Shirokov@cmgdigital.com>
2010-09-19 19:25:32 -0400

    [master] I'm an idiot that has no idea how to use setUp

--- tests.py
+++ tests.py
@@ -7,9 +7,9 @@ import unittest
 import gogogo
 
 def default_setUp(self):
-    def get_default_board():
-        return gogogo.BoardState()
-    self.get_default_board = get_default_board
+    self.new_board = lambda *args, **kwargs: gogogo.BoardState(*args, **kwargs)
+    self.board = self.new_board()
+
 
 class SimpleBoardStateTests(unittest.TestCase):
     setUp = default_setUp
@@ -18,45 +18,39 @@ class SimpleBoardStateTests(unittest.TestCase):
         pass
 
     def test_distance_function(self):
-        board = self.get_default_board()
-        self.assertEqual(int(board._distance((0,0), (0, 1))), 1, "Two points are 1 unit appart")
-        self.assertEqual(int(board._distance((0,0), (0, 2))), 2, "Two points are 2 unit appart")
-        self.assertEqual(int(board._distance((0,0), (1, 1))), 1, "Two points are 1 unit appart by diagonal")
-        self.assertEqual(int(board._distance((0,0), (0, 0))), 0, "Two points the same")
+        self.assertEqual(int(self.board._distance((0,0), (0, 1))), 1, "Two points are 1 unit appart")
+        self.assertEqual(int(self.board._distance((0,0), (0, 2))), 2, "Two points are 2 unit appart")
+        self.assertEqual(int(self.board._distance((0,0), (1, 1))), 1, "Two points are 1 unit appart by diagonal")
+        self.assertEqual(int(self.board._distance((0,0), (0, 0))), 0, "Two points the same")
 
     def test_two_players(self):
-        board = self.get_default_board()
-        self.assertEqual(len(board.players), 2, "Game needs two players")
+        self.assertEqual(len(self.board.players), 2, "Game needs two players")
 
     def test_board_has_size(self):
-        board = self.get_default_board()
-        self.assertTrue(board.width > 0, "Board needs to have a width")
-        self.assertTrue(board.height > 0, "Board needs to have a height")
+        self.assertTrue(self.board.width > 0, "Board needs to have a width")
+        self.assertTrue(self.board.height > 0, "Board needs to have a height")
 
     def test_intersection_has_value(self):
-        board = self.get_default_board()
-        board._set(0, 0, "Black")
-        self.assertEqual(type(board._get(0, 0)), gogogo.Position, "Value of intersection is returnable.")
+        self.board._set(0, 0, "Black")
+        self.assertEqual(type(self.board._get(0, 0)), gogogo.Position, "Value of intersection is returnable.")
 
     def test_intersection_only_takes_valid_state(self):
-        new_board = lambda: self.get_default_board()
+        new_board = lambda: gogogo.BoardState()
         self.assertTrue(new_board()._set(0, 0, None), "A space should be able to be clear")
         self.assertTrue(new_board()._set(0, 0, "Black"), "A space should be able to be black")
         self.assertTrue(new_board()._set(0, 0, "White"), "A space should be able to be white")
 
     def test_intersection_can_only_have_one_value(self):
-        board = self.get_default_board()
-        board._set(0, 0, "Black")
-        board._set(0, 0, "White")
-        self.assertEqual(len(board.positions), 1, "A space can only have one position")
+        self.board._set(0, 0, "Black")
+        self.board._set(0, 0, "White")
+        self.assertEqual(len(self.board.positions), 1, "A space can only have one position")
 
     def test_blank_positions_are_not_stored(self):
-        board = self.get_default_board()
-        board._set(0, 0, None)
-        self.assertEqual(len(board.positions), 0, "We should not be keeping track of empty squares")
-        board._set(1, 1, "Black")
-        board._set(1, 1, None)
-        self.assertEqual(len(board.positions), 0, "Clearing a square should remove the position.")
+        self.board._set(0, 0, None)
+        self.assertEqual(len(self.board.positions), 0, "We should not be keeping track of empty squares")
+        self.board._set(1, 1, "Black")
+        self.board._set(1, 1, None)
+        self.assertEqual(len(self.board.positions), 0, "Clearing a square should remove the position.")
 
 class BoardStateSimpleGroupTests(unittest.TestCase):
     setUp = default_setUp
@@ -65,28 +59,24 @@ class BoardStateSimpleGroupTests(unittest.TestCase):
         pass
 
     def test_two_interersections_are_connected(self):
-    	board = self.get_default_board()
-        self.assertTrue(board.is_chain((1,1),(1,2)), "Intersections are connected if a path exists between them")
-        self.assertTrue(board.is_chain((1,1),(3,6)), "Intersections are connected if a longer path exists between them")
+        self.assertTrue(self.board.is_chain((1,1),(1,2)), "Intersections are connected if a path exists between them")
+        self.assertTrue(self.board.is_chain((1,1),(3,6)), "Intersections are connected if a longer path exists between them")
 
     def test_two_intersections_not_connected(self):
-        board = self.get_default_board()
         b, w = "Black", "White"
-        board._set(0, 1, b)
-        board._set(1, 0, b)
-        self.assertFalse(board.is_chain((0,0),(1,1)), "Two intersections with no path without diagonals of equal state are not connected")
+        self.board._set(0, 1, b)
+        self.board._set(1, 0, b)
+        self.assertFalse(self.board.is_chain((0,0),(1,1)), "Two intersections with no path without diagonals of equal state are not connected")
 
     def test_two_stones_connected(self):
-        board = self.get_default_board()
-        board._set(5, 5, 'Black')
-        board._set(5, 6, 'Black')
-        self.assertTrue(board.is_chain((5,5), (5,6)), "Two 90deg adjacent stones are connected")
+        self.board._set(5, 5, 'Black')
+        self.board._set(5, 6, 'Black')
+        self.assertTrue(self.board.is_chain((5,5), (5,6)), "Two 90deg adjacent stones are connected")
 
     def test_two_diagonal_stones_not_connected(self):
-        board = self.get_default_board()
-        board._set(5, 5, 'Black')
-        board._set(6, 6, 'Black')
-        self.assertFalse(board.is_chain((5,5), (6,6)), "Two 90deg adjacent stones are connected")
+        self.board._set(5, 5, 'Black')
+        self.board._set(6, 6, 'Black')
+        self.assertFalse(self.board.is_chain((5,5), (6,6)), "Two 90deg adjacent stones are connected")
 
 class BoardStateTests(unittest.TestCase):
     setUp = default_setUp
@@ -95,86 +85,78 @@ class BoardStateTests(unittest.TestCase):
         pass
     
     def test_complex_shape_is_connected(self):
-        board = self.get_default_board()
         b = 'Black'
-        all = [board._set(0, 0, b),
-               board._set(0, 1, b),
-               board._set(1, 1, b),
-               board._set(1, 2, b)]
+        all = [self.board._set(0, 0, b),
+               self.board._set(0, 1, b),
+               self.board._set(1, 1, b),
+               self.board._set(1, 2, b)]
         all_connected = reduce(lambda acc, p: acc and
                                               (len([t for t in all
-                                                      if board.is_chain((p.x, p.y), (t.x, t.y))]) == len(all)),
+                                                      if self.board.is_chain((p.x, p.y), (t.x, t.y))]) == len(all)),
                                all, True)
         self.assertTrue(all_connected, "A shape of three pairs at 90deg angles, all peices are connected")
 
     def test_shape_knows_all_members(self):
-        board = self.get_default_board()
-        members = [board._set(x, y, "Black") for (x, y) in
+        members = [self.board._set(x, y, "Black") for (x, y) in
                    [(0,0),
                     (0,1),
                     (1, 0)]]
-        shape = board.shape_at(0, 0)
+        shape = self.board.shape_at(0, 0)
         all_present = len(members) == len([m for m in shape.members if m in members]) == shape.size
         self.assertTrue(all_present, "A shape should be able to tell me its members")
 
     def test_two_complex_shapes_are_distinct(self):
-        board = self.get_default_board()
-        [board._set(x, y, "Black") for (x, y)
+        [self.board._set(x, y, "Black") for (x, y)
          in [(10, 10),
              (9, 10),
              (8, 10),
              (7, 9),
              (6, 9),
              (5, 9)]]
-        shape1 = board.shape_at(10,10)
+        shape1 = self.board.shape_at(10,10)
         self.assertTrue(shape1, "There is a shape that touches (10, 10)")
-        shape2 = board.shape_at(6, 9)
+        shape2 = self.board.shape_at(6, 9)
         self.assertTrue(shape2, "There is a shape that touches (6, 9)")
         self.assertNotEqual(shape1, shape2, "Two shapes are present because of a diagonal")
 
     def test_liberty_is_adjecent_empty_space_of_stone(self):
-        board = self.get_default_board()
-        p = board._set(1, 1, "Black")
-        board._set(0, 1, "Black")
+        p = self.board._set(1, 1, "Black")
+        self.board._set(0, 1, "Black")
         self.assertTrue(p.is_liberty(1, 0), "A Liberty is an adjecent empty space of stone")
         self.assertFalse(p.is_liberty(0, 1), "A Liberty is an adjecent _empty_ space of stone")
 
     def test_single_stone_has_four_liberties(self):
-        board = self.get_default_board()
-        p = board._set(1, 1, "Black")
+        p = self.board._set(1, 1, "Black")
         self.assertEqual(len(p.liberties), 4, "A single stone has four liberties")
 
     def test_single_oposing_stone_removes_liberty(self):
-        board = self.get_default_board()
-        p = board._set(1, 1, "Black")
+        p = self.board._set(1, 1, "Black")
         before = p.liberties
-        board._set(0, 1, "White")
+        self.board._set(0, 1, "White")
         after = p.liberties
         self.assertEqual(len(after), len(before) - 1, "An adjacent oposing stone, decrements a stone's liberties")
 
     def test_group_shares_liberties(self):
-        board = self.get_default_board()
-        board._set(10, 10, "Black")
-        board._set(10, 11, "Black")
-        board._set(9, 10, "Black")
-        shape = board.shape_at(10, 10)
+        self.board._set(10, 10, "Black")
+        self.board._set(10, 11, "Black")
+        self.board._set(9, 10, "Black")
+        shape = self.board.shape_at(10, 10)
         self.assertEqual(len(shape.liberties), 7, "Adding a connected stone shares the liberties of both stones")
 
     def test_shape_can_count_liberties(self):
-        board = self.get_default_board()
-        board._set(10, 10, "Black")
-        board._set(10, 11, "Black")
-        board._set(9, 10, "Black")
-        board._set(8, 10, "Black")
-        board._set(8, 11, "Black")
-        board._set(8, 12, "Black")
-        board._set(8, 13, "Black")
-        board._set(7, 12, "Black")
+        self.board._set(10, 10, "Black")
+        self.board._set(10, 11, "Black")
+        self.board._set(9, 10, "Black")
+        self.board._set(8, 10, "Black")
+        self.board._set(8, 11, "Black")
+        self.board._set(8, 12, "Black")
+        self.board._set(8, 13, "Black")
+        self.board._set(7, 12, "Black")
 
-        board._set(7, 10, "White")
-        board._set(9, 11, "White")
+        self.board._set(7, 10, "White")
+        self.board._set(9, 11, "White")
 
-        shape = board.shape_at(10, 10)
+        shape = self.board.shape_at(10, 10)
 
         self.assertEqual(len(shape.liberties), 12, "A shape should correctly count its liberties")
 
