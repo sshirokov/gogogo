809cd1a5fc7f6ec7b4bc7fe4158f185233d899b1
Yaroslav Shirokov <slava@slavasaur.com>
2010-11-26 16:12:53 -0500

    [web-client] Drawing the game grid

--- media/web/static/js/board.js
+++ media/web/static/js/board.js
@@ -1,4 +1,17 @@
 (function($) {
+     var gfx = window._gfx = {
+         paper: undefined,
+         container: '#game',
+         width: 500,
+         height: 500,
+         rows: 19,
+         cols: 19,
+
+         elements: {
+
+         }
+     };
+
      function load_board(url) {
          console.log("Loading board from:", url);
          $.ajax({url: url,
@@ -17,9 +30,59 @@
                  }
          });
      }
-     //Create paper
-     window.gogogo.paper = Raphael($('#game').get(0), 500, 500);
-     var c = window.gogogo.paper.circle(250, 250, 50);
-     c.attr('fill', "#ff0000");
+
+     function draw_board(board) {
+         gfx.paper.clear();
+         gfx.elements = {};
+
+         //Draw underling board
+         gfx.elements.base = gfx.paper.rect(10, 10, 480, 480, 5);
+         gfx.elements.base.attr('fill', "#D1D190");
+         gfx.elements.lines = [];
+
+         console.log("Drawing cols");
+         for(var col = 0; col < gfx.cols; col++) {
+             var ex, ey,
+                 sx = 30,
+                 sy = 30,
+                 distance = gfx.width - (sx * 2),
+                 step = distance / (gfx.cols - 1);
+             ey = distance + sx;
+             sx += (step * col);
+             ex = sx;
+             var path = "M" + sx + " " + sy + "L" + ex + " " + ey;
+             gfx.elements.lines.push(gfx.paper.path(path));
+         }
+
+         console.log("Drawing rows");
+         for(var row = 0; row < gfx.rows; row++) {
+             var ex, ey,
+                 sx = 30,
+                 sy = 30,
+                 distance = gfx.width - (sy * 2),
+                 step = distance / (gfx.rows - 1);
+             ex = distance + sx;
+             sy += (step * row);
+             ey = sy;
+             var path = "M" + sx + " " + sy + "L" + ex + " " + ey;
+             console.log("Path:", path);
+             gfx.elements.lines.push(gfx.paper.path(path));
+         }
+
+     }
+
+     //Unpublished
+     function init_board() {
+         //Create paper
+         gfx.paper = Raphael($(gfx.container).get(0), gfx.width, gfx.height);
+
+     }
+
+     //Publish and API
      window.gogogo.load_board = load_board;
+     window.gogogo.draw_board = draw_board;
+
+     //Boot
+     init_board();
+     draw_board();
 })(jQuery);
