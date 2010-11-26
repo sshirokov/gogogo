faac754c0110ec451e3580f52745a2652ca6706f
Yaroslav Shirokov <slava@slavasaur.com>
2010-11-26 08:13:37 -0500

    [web-client] Rendering the form again, playing with the forms a little

--- media/web/static/js/app.js
+++ media/web/static/js/app.js
@@ -1,16 +1,31 @@
 window.gogogo = {
     query: undefined,
     boot: function($) {
+        window.gogogo.query = $;
         $.ajaxSetup({
                         contentType: 'application/json',
                         dataType: 'json'
                     });
-        window.gogogo.query = $;
+    },
+
+    show_screens: function() {
+        var $ = window.gogogo.query;
+
+        $('.screen').removeClass('visible');
+
+        if(window.location.hash)
+            $('.screen' + window.location.hash).addClass('visible');
+        else
+            window.location.hash = $('.screen.default').attr('id');
+
+        if($('.screen.visible').length == 0)
+            $('.screen.default').addClass('visible');
     }
 };
 
 (function($) {
      $(function() {
            window.gogogo.boot($);
+           window.gogogo.show_screens();
      });
 })(jQuery);
