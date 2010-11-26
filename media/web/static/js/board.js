c0e5b3cd5e58fc593383146a786631ba6232876d
Yaroslav Shirokov <slava@slavasaur.com>
2010-11-26 14:58:54 -0500

    [web-client] Fetching game

--- media/web/static/js/board.js
+++ media/web/static/js/board.js
@@ -1,6 +1,21 @@
 (function($) {
      function load_board(url) {
          console.log("Loading board from:", url);
+         $.ajax({url: url,
+                 type: 'GET',
+
+                 success: function(data, text_status, xhr) {
+                     console.log("We win a board:", data, text_status, xhr, xhr.getResponseHeader('location'));
+
+                     return false;
+                 },
+
+                 error: function(xhr, text_status, errorThrown) {
+                     console.log("We lose:", xhr, text_status, errorThrown);
+
+                     return false;
+                 }
+         });
      }
      window.gogogo.load_board = load_board;
 })(jQuery);
